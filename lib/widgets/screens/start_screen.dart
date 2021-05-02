f02547f6a852133008c60183eaf867b54370a5d2
fukky21 <r.fukky21@gmail.com>
2021-05-03 01:48:58 +0900

    サインアウト時にモーダルをかける

--- lib/widgets/screens/start_screen.dart
+++ lib/widgets/screens/start_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:provider/provider.dart';
 
+import '../../notifiers/index.dart';
 import '../../utils/index.dart';
 import '../../widgets/tabs/index.dart';
 
@@ -25,67 +28,73 @@ class _StartScreenState extends State<StartScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // ここでConsumer<AuthenticationNotifier>(...)を使用してはいけない
-    // 以下のエラーが発生する
-    // Unhandled Exception: A HomeTabStateNotifier was used after being disposed
-    return Scaffold(
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _tabs,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              FontAwesomeIcons.home,
-              size: _iconSize,
+    return Consumer<AuthenticationNotifier>(
+      builder: (context, notifier, _) {
+        final state = notifier?.state ?? AuthenticationInProgress();
+
+        return ModalProgressHUD(
+          inAsyncCall: state is AuthenticationInProgress,
+          child: Scaffold(
+            body: IndexedStack(
+              index: _currentIndex,
+              children: _tabs,
             ),
-            label: 'ホーム',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              FontAwesomeIcons.search,
-              size: _iconSize,
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: _currentIndex,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    FontAwesomeIcons.home,
+                    size: _iconSize,
+                  ),
+                  label: 'ホーム',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    FontAwesomeIcons.search,
+                    size: _iconSize,
+                  ),
+                  label: 'さがす',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    FontAwesomeIcons.solidHeart,
+                    size: _iconSize,
+                  ),
+                  label: 'お気に入り',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    FontAwesomeIcons.solidBell,
+                    size: _iconSize,
+                  ),
+                  label: 'お知らせ',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    FontAwesomeIcons.solidUser,
+                    size: _iconSize,
+                  ),
+                  label: 'マイページ',
+                ),
+              ],
+              backgroundColor: AppColors.grey10,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              type: BottomNavigationBarType.fixed,
+              onTap: (index) {
+                if (index == 0 && _currentIndex == index) {
+                  // ホームタブ表示中にホームアイコンがタップされたとき
+                  homeTabScrollToTop();
+                }
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
             ),
-            label: 'さがす',
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              FontAwesomeIcons.solidHeart,
-              size: _iconSize,
-            ),
-            label: 'お気に入り',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              FontAwesomeIcons.solidBell,
-              size: _iconSize,
-            ),
-            label: 'お知らせ',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              FontAwesomeIcons.solidUser,
-              size: _iconSize,
-            ),
-            label: 'マイページ',
-          ),
-        ],
-        backgroundColor: AppColors.grey10,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.fixed,
-        onTap: (index) {
-          if (index == 0 && _currentIndex == index) {
-            // ホームタブ表示中にホームアイコンがタップされたとき
-            homeTabScrollToTop();
-          }
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-      ),
+        );
+      },
     );
   }
 }
